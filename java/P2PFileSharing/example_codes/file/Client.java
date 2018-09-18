f3f37d1d2be1cc990490792fdf94110566d8550d
liyu10000 <yuli00986081@gmail.com>
2018-09-18 20:55:42 +0800

    packed header info before file

--- java/P2PFileSharing/example_codes/file/Client.java
+++ java/P2PFileSharing/example_codes/file/Client.java
@@ -5,6 +5,10 @@ import java.net.Socket;
 
 public class Client {
 	private Socket client;
+	String directory;
+	String filename;
+	int length;
+
 
 	public Client(String host, int port) {
 		try {
@@ -15,33 +19,55 @@ public class Client {
 		}
 	}
 
-	public void saveFile(String filename) {
-		try {
-			DataInputStream dis = new DataInputStream(client.getInputStream());
-			FileOutputStream fos = new FileOutputStream(filename);
-
-			byte[] b = new byte[4096];
-			int filesize = 1024000;
-			int read = 0;
-			int totalRead = 0;
-			int remaining = filesize;
-			while ((read = dis.read(b, 0, Math.min(b.length, remaining))) > 0) {
-				totalRead += read;
-				remaining -= read;
-				fos.write(b, 0, read);
+	public void saveHead(Socket socket) throws IOException {
+		DataInputStream dis = new DataInputStream(socket.getInputStream());
+		byte signal = dis.readByte();
+		while (signal != -1) {
+			signal = dis.readByte();
+			switch(signal) {
+				case 1:
+					directory = dis.readUTF();
+					break;
+				case 2:
+					filename = dis.readUTF();
+					break;
+				case 3:
+					length = dis.readInt();
+					break;
+				default:
+					signal = -1;
 			}
-			System.out.println("Read " + filename + " from server.");
-			if (fos != null) fos.close();
-			if (dis != null) dis.close();
-
-			System.out.println("\nClient session ended.");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
 		}
+		System.out.print("head saved.");
+	}
+
+	public void saveBody(Socket socket) throws IOException {
+		DataInputStream dis = new DataInputStream(socket.getInputStream());
+		FileOutputStream fos = new FileOutputStream(filename);
+
+		byte[] b = new byte[4096];
+		int filesize = length;
+		int read = 0;
+		int totalRead = 0;
+		int remaining = filesize;
+		while ((read = dis.read(b, 0, Math.min(b.length, remaining))) > 0) {
+			totalRead += read;
+			remaining -= read;
+			fos.write(b, 0, read);
+		}
+		
+		fos.close();
+		dis.close();
+	}
+
+	public void save(String filename) throws IOException {
+		saveHead(client);
+		saveBody(client);
+		System.out.println("Read " + filename + " from server.");
+		System.out.println("\nClient session ended.");
 	}
 
 	public static void main(String[] args) {
-		(new Client(args[0], Integer.valueOf(args[1]))).saveFile(args[2]);
+		(new Client(args[0], Integer.valueOf(args[1]))).save(args[2]);
 	}
 }
--- java/P2PFileSharing/example_codes/file/Server.java
+++ java/P2PFileSharing/example_codes/file/Server.java
@@ -18,34 +18,62 @@ public class Server {
 		}
 	}
 
-	public void sendFile(String filename) {
-		try {
-			Socket client = server.accept();
-			System.out.println("Client connected.");
-			
-			File f = new File(filename);
-			byte[] b = new byte[(int) f.length()];
-			FileInputStream fis = new FileInputStream(f);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			bis.read(b, 0, b.length);
-			DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-			dos.write(b, 0, b.length);
-			dos.flush();
-			System.out.println("Sent " + filename + " to client.");
+	public void sendHead(Socket socket, String directory, String filename, long length) throws IOException {
+		DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+		
+		// start
+		dos.writeByte(0);
+		dos.flush();
+		// directory
+		dos.writeByte(1);
+		dos.writeUTF(directory);
+		dos.flush();
+		// filename
+		dos.writeByte(2);
+		dos.writeUTF(filename);
+		dos.flush();
+		// file length
+		dos.writeByte(3);
+		dos.writeInt((int)length);
+		dos.flush();
+		// end
+		dos.writeByte(-1);
+		dos.flush();
 
-			if (fis != null) fis.close();
-			if (bis != null) bis.close();
-			if (dos != null) dos.close();
-			if (client != null) client.close();
+		dos.close();
 
-			System.out.println("\nServer session ended.");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
+		System.out.println("head sent.");
 	}
 
-	public static void main(String[] args) {
-		(new Server(Integer.valueOf(args[0]))).sendFile(args[1]);
+	public void sendBody(Socket socket, String filename) throws IOException {
+		File f = new File(filename);
+		byte[] b = new byte[(int) f.length()];
+		FileInputStream fis = new FileInputStream(f);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		bis.read(b, 0, b.length);
+		DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+		dos.write(b, 0, b.length);
+		dos.flush();
+		System.out.println("body sent.");
+
+		fis.close();
+		bis.close();
+		dos.close();
+	}
+
+	public void send(String filename) throws IOException {
+		Socket client = server.accept();
+		System.out.println("Client connected.");
+
+		File f = new File(filename);
+		sendHead(client, "/home", filename, f.length());
+		sendBody(client, filename);
+
+		client.close();
+		System.out.println("\nServer session ended.");
+	}
+
+	public static void main(String[] args) throws IOException {
+		(new Server(Integer.valueOf(args[0]))).send(args[1]);
 	}
 }
