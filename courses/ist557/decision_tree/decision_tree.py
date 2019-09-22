f8d5578f43ad7989b4ff668d394215ef62a6b273
liyu10000 <yuli00986081@gmail.com>
2019-09-22 00:18:10 -0400

    started to implement decision tree classifier

--- courses/ist557/decision_tree/decision_tree.py
+++ courses/ist557/decision_tree/decision_tree.py
@@ -1,7 +1,31 @@
 """
 Reference site link: https://medium.com/@penggongting/implementing-decision-tree-from-scratch-in-python-c732e7c69aea.
 """
+import numpy as np
 
 
 def entropy():
 	pass
+
+class DecisionTreeClassifier:
+	def fit(self, max_depth, min_sample_leaf):
+		self.dtree = Node(X, y, np.arange(len(y)), max_depth, min_sample_leaf)
+		return self
+
+	def predict(self, X):
+		return self.dtree.predict(X)
+
+class Node:
+	def __init__(self, X, y, index, max_depth, min_sample_leaf):
+		self.X = X
+		self.y = y
+		self.index = index
+		self.max_depth = max_depth
+		self.min_sample_leaf = min_sample_leaf
+		self.row_count = len(index)
+		self.col_count = X.shape[1]
+		self.val = np.mean(y[index])
+		self.score = float('inf')
+		self.find_varsplit()
+
+	
